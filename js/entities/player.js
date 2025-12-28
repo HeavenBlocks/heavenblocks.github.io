f5ad425a68459e2645a8ffa56bf07c8aa9393fa3
HeavenBlocks <HackAndGame@protonmail.com>
2025-12-28 02:21:58 -0500

    Update player.js

--- js/entities/player.js
+++ js/entities/player.js
@@ -1,14 +1,30 @@
 import { scene } from "../core/scene.js";
 
+const loader = new THREE.TextureLoader();
+
+const faceTexture = loader.load(
+    "https://cdn.jsdelivr.net/gh/HeavenBlocks/heavenblocks.github.io/pngs/face.png"
+);
+
+const materials = [
+    new THREE.MeshStandardMaterial({ color: 0xff5555 }), // right
+    new THREE.MeshStandardMaterial({ color: 0xff5555 }), // left
+    new THREE.MeshStandardMaterial({ color: 0xff5555 }), // top
+    new THREE.MeshStandardMaterial({ color: 0xff5555 }), // bottom
+    new THREE.MeshStandardMaterial({ map: faceTexture, transparent: true }), // front
+    new THREE.MeshStandardMaterial({ color: 0xff5555 })  // back
+];
+
 export const player = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshStandardMaterial({ color: 0xff4444 })
+    materials
 );
 
 player.position.set(0, 2, 0);
 scene.add(player);
 
 export const playerState = {
-    velocityY: 0,
-    onGround: false
+    velocity: new THREE.Vector3(),
+    onGround: false,
+    coyoteTime: 0
 };
